9ca4beaad72c01666e33a85e446088c49ad5323a
Izan Alejo Pérez <izanalejo@MacBook-Pro-de-Izan.local>
2025-11-25 12:34:12 +0100

    Practica 2.2

--- Pt2.1_FDW/resources/views/partials/nav.blade.php
+++ Pt2.1_FDW/resources/views/partials/nav.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Ritmo y Movimiento</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page"href="{{ url('/') }}">Inici</a>
+          <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Inici</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ url('/serveis') }}">Serveis</a>
@@ -16,10 +16,13 @@
           <a class="nav-link" href="{{ url('/about') }}">Sobre nosaltres</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ url('/contacte') }}">Contacte</a>
+          <a class="nav-link" href="{{ route('contacte.pagina') }}">Contacte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/management/estadistiques') }}">Estadístiques</a>
         </li>
       </ul>
-      <a class="navbar-brand mx-auto" style="color: white">WEB CORPORATIVA LARAVEL</a>
+      <a class="navbar-brand mx-auto" style="color: white">WEB CORPORATIVA DE LARAVEL</a>
     </div>
   </div>
 </nav>
--- Pt2.1_FDW/resources/views/serveis.blade.php
+++ Pt2.1_FDW/resources/views/serveis.blade.php
@@ -1,39 +1,20 @@
 @extends('layouts.app')
 @section('titol', 'Serveis')
 @section('contingut')
-    <!-- Sección de clases -->
     <div style="border-radius: 20px;" class="container">
-        <h1 style="text-align: center; color: whitesmoke; text-shadow: 2px 2px 5px #056d71ec;" class="mb-4 text-center">Nuestras Clases</h1><hr>
+        <h1 style="text-align: center; color: whitesmoke; text-shadow: 2px 2px 5px #056d71ec;" class="mb-4 text-center">Els nostres Serveis</h1><hr>
+        
         <div class="row">
-            <div class="col-md-4 mb-4">
-                <div style="background-color: #056d71ec;" class="card h-100">
-                    <img src="{{URL::asset('img/salsa.png')}}" class="card-img-top" alt="Salsa" width="300" height="500">
-                    <div class="card-body">
-                        <h5 class="card-title">Salsa</h5>
-                        <p class="card-text">Aprende los movimientos más sensuales y divertidos de la salsa con nuestros instructores expertos.</p>
+            @foreach($serveis as $servei)
+                <div class="col-md-4 mb-4">
+                    <div style="background-color: #056d71ec; color: whitesmoke;" class="card h-100">
+                        <div class="card-body">
+                            <h5 class="card-title" style="font-weight: bold; font-size: 1.3em;">{{ $servei['nom'] }}</h5>
+                            <p class="card-text">{{ $servei['descripcio'] }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div style="background-color: #056d71ec;" class="card h-100">
-                    <img src="{{URL::asset('img/ballet.png')}}" class="card-img-top" alt="Ballet" width="300" height="500">
-                    <div class="card-body">
-                        <h5 class="card-title">Ballet</h5>
-                        <p class="card-text">Desarrolla fuerza, flexibilidad y gracia a través del ballet clásico, desde iniciación hasta avanzado.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div style="background-color: #056d71ec;" class="card h-100">
-                    <img src="{{URL::asset('img/hiphop.png')}}" class="card-img-top" alt="Hip Hop" width="300" height="500">
-                    <div class="card-body">
-                        <h5 class="card-title">Hip-Hop</h5>
-                        <p class="card-text">Exprésate con estilo y energía. Aprende pasos urbanos y coreografías modernas que te harán brillar.</p>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
-
 @endsection
-
